db03672646c5a1583ca04c12967e6d58ad52313b
David Jeyachandran <davidjeyachandran@gmail.com>
2024-01-20 11:15:36 +1100

    Simple tone field

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { encouragementData } from './data';
+import { Button, Container, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const [topic, setTopic] = useState('');
+  const [randomNumber, setRandomNumber] = useState(0);
+  const topicSet = new Set(encouragementData.map((encouragement) => encouragement.TOPIC));
+  const topicList = Array.from(topicSet);
+
+  const messages = encouragementData.filter((encouragement) => encouragement.TOPIC === topic);
+  const randomMessage = messages[randomNumber];
+  const handleChange = (event) => {
+    setTopic(event.target.value)
+  }
+
+  const handleClick = () => {
+    setRandomNumber(Math.floor(Math.random() * messages.length));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Typography variant="h2" sx={{ my: 4 }}>
+        Encouragement App
+      </Typography>
+      <FormControl fullWidth sx={{ my: 4 }}>
+        <InputLabel id="demo-simple-select-label">Topic</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={topic}
+          label="Topic"
+          onChange={handleChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {topicList.map((topic) => <MenuItem key={topic} value={topic}>{topic}</MenuItem>)}
+        </Select>
+      </FormControl>
+      <Typography variant="h5" sx={{ my: 4 }}>
+        You have picked the topic "{topic}"
+      </Typography>
+
+      <Button variant="contained" fullWidth onClick={handleClick} sx={{ my: 4 }}>
+        New Quote
+      </Button>
+
+      <Typography variant="p" sx={{ my: 4 }}>
+        {randomMessage?.MESSAGE}
+      </Typography>
+    </Container>
   );
 }
 
